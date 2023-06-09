4e2b33472d6703d792a6c4542d1345f435e1cde2
JonaCaucota <jonacau12@gmail.com>
2023-06-09 15:31:42 -0300

    Added checks for POST to usuers

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,8 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
+const Usuario = require('../models/usuario');
+const {validationResult} = require("express-validator");
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -15,14 +18,37 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
 
-    const { nombre, edad } = req.body;
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({
+        nombre, correo, password, rol
+    });
+
+    //Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        return res.status(400).json({
+            msg: 'Ese correo ya esta registrado'
+        })
+    }
+
+    //Encriptar contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //Guardo usuario
+    usuario.save();
 
     res.json({
         msg: 'post API - usuariosPost',
-        nombre, 
-        edad
+        usuario
     });
 }
 
--- database/config.js
+++ database/config.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').default;
 
 const dbConnection = async () => {
     try {
